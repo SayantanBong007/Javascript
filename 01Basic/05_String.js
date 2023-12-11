dd87fd51db57f24af0c7005cc621d27a1c43a063
Sayantan Gain <140269673+SayantanBong007@users.noreply.github.com>
2023-12-11 14:24:57 +0000

    string

---
+++ 01Basic/05_String.js
@@ -0,0 +1,21 @@
+const name="sam"
+const age=21
+console.log(`Hello my name ${name} and my age is ${age}`);
+
+const nam=new String('kakakaak')
+
+console.log(nam.length)
+console.log(nam.toUpperCase());
+console.log(nam.charAt(2))
+console.log(nam.indexOf('a'));
+
+const newStr=nam.substring(0,4)
+console.log(newStr);
+
+const news="  jio   "
+console.log(news)
+console.log(news.trim())
+
+const url="http/hsdis-dnd%20s-dasdj.com"
+console.log(url.replace('%20','-'))
+console.log(url.split('-'));
--- 01Basic/dataType_summary.js
+++ 01Basic/dataType_summary.js
@@ -7,8 +7,8 @@
 //Array, Object, Function
 
 let myObj={
-    name:Sam
-    age:21
+    name:"sam",
+    age: 21,
 }
 
 /* interview related questions 
@@ -39,3 +39,16 @@ let myObj={
 - typeof datatyped is available on documentation 
 
 */
+
+//++++++++++++++++++++++++++++++++++++
+
+//Stack(Primitive) , Heap(Non-Primitive)
+
+/* - Primitive data type goes to Stack we get a copy of that value.
+- Non-Primitive data type goes to Heap we get refrence of that value.  */
+
+/*Primitive types like numbers, strings, etc., are straightforward. They go to the stack, and when you work with them, you're actually working directly with the value itself. It's like having a copy of that value in your hand.
+
+Non-primitive types like objects or arrays are more complex. They go to the heap, and when you work with them, you're actually working with a reference to where that value is stored in memory. It's like having a signpost that tells you where to find the value rather than holding the value directly.
+
+So, primitives are like having the thing itself, while non-primitives are like having directions to where the thing is kept. */
