4064f34c0b800e32ea4b1c2140508350fc7047cf
Sayantan Gain <140269673+SayantanBong007@users.noreply.github.com>
2023-12-11 10:32:16 +0000

    Conversion of  data type

---
+++ 01Basic/02_Datatype.js
@@ -0,0 +1,11 @@
+"Use strict";//treat all js code as newer version
+
+//number=>2 to power 533
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+console.log(typeof null); //null is a object
